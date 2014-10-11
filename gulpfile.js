c357ec0ce50710fe9cc3bf1019d7a2559007f9f9
daikissdd <daikissdd@gmail.com>
2014-10-11 23:04:51 +0700

    build_update

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,27 @@
 var gulp = require('gulp'),
+	_ = require('underscore'),
 	concat = require('gulp-concat'),
 	uglify = require('gulp-uglify'),
 	minifyCss = require('gulp-minify-css'),
 	imagemin = require('gulp-imagemin'),
 	replace = require('gulp-replace'),
 	minifyHTML = require('gulp-minify-html'),
+	template = require('gulp-template'),
 	shell = require('gulp-shell');
 
-gulp.task('shell', shell.task([
+gulp.task('git', shell.task([
+	'git add -A',
+	'git commit -m"build_update"',
+	'git push origin gh-pages'
+]));
+
+gulp.task('new', shell.task([
 	'git add -A',
 	'git commit -m"build_update"',
 	'git push origin gh-pages'
 ]));
 
 gulp.task('default', function() {
-	gulp.run('shell');
+	
+	gulp.run('git');
 });
